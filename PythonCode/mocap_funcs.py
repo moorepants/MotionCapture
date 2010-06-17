31fae0f6b848657e45eff574a5893b412305ab42
Jason Moore <moorepants@gmail.com>
2010-06-17 13:11:11 -0700

    starting a module for the mocap functions

---
+++ PythonCode/mocap_funcs.py
@@ -0,0 +1,27 @@
+
+def uniquify(seq):
+    # Not order preserving
+    keys = {}
+    for e in seq:
+        keys[e] = 1
+    return keys.keys()
+
+def findall(L, value):
+    '''
+    Finds all the indices of a value in a list.
+
+    Parameters:
+    -----------
+    L : list
+    value : the search value
+
+    Returns:
+    --------
+    a : list of indices that match value
+
+    '''
+    a = []
+    for i in range(len(L)):
+        if L[i] == value:
+            a.append(i)
+    return a
