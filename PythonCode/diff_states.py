a5ecce63a5c798e53c880444bb48aba008796720
Jason Moore <moorepants@gmail.com>
2010-06-28 13:27:11 -0700

    cleaned up the diff_states code a bit

--- PythonCode/diff_states.py
+++ PythonCode/diff_states.py
@@ -10,18 +10,18 @@ f.close()
 
 fstats = {}
 
+sr = 100 # sample rate
+
 for run in runInfo['run']:
     q = load('../data/npy/states/' + run + 'q.npy')
     l = q.shape[1]
-    t = linspace(0, l/100, num=l)
-    qd = derivative(t, q)
-    qd = butterworth(qd, 10., 100.)
-    qdd = derivative(t[:-1], qd)
-    qdd = butterworth(qdd, 10., 100.)
+    t = linspace(0, l/sr, num=l)
+    qd = butterworth(derivative(t, q), 10., sr)
+    qdd = butterworth(derivative(t[:-1], qd), 10., sr)
     # calculate the frequency spectrums of each data set
-    qF, qA = freq_spectrum(100, q)
-    qdF, qdA = freq_spectrum(100, qd)
-    qddF, qddA = freq_spectrum(100, qdd)
+    qF, qA = freq_spectrum(sr, q)
+    qdF, qdA = freq_spectrum(sr, qd)
+    qddF, qddA = freq_spectrum(sr, qdd)
     # calculate the statistics of the frequency spectrums
     fstats['q'] = curve_area_stats(qF, qA)
     fstats['qd'] = curve_area_stats(qdF, qdA)
