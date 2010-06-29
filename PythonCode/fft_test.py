i think the fft scaling is correct now

PythonCode/fft_test.py
@@ -4,24 +4,24 @@ from mocap_funcs import *
 from scipy.integrate import trapz, cumtrapz
 
 # compute the frequency spectrum of example configuration variables
-q = np.load('../data/npy/states/2042q.npy')
-f, a = freq_spectrum(100, q)
-stats = curve_area_stats(f, a)
-for i, variable in enumerate(a.T):
-    legend = ['Frequency Spectrum']
-    plt.figure(i)
-    plt.plot(f, variable)
-    for percent, value in stats.items():
-        plt.plot([value[i], value[i]], [0, np.max(variable)], linewidth=2)
-        legend.append(percent)
-    plt.axis('tight')
-    plt.legend(legend)
-    plt.xlim([0, 5])
+#q = np.load('../data/npy/states/2042q.npy')
+#f, a = freq_spectrum(100, q)
+#stats = curve_area_stats(f, a)
+#for i, variable in enumerate(a.T):
+    #legend = ['Frequency Spectrum']
+    #plt.figure(i)
+    #plt.plot(f, variable)
+    #for percent, value in stats.items():
+        #plt.plot([value[i], value[i]], [0, np.max(variable)], linewidth=2)
+        #legend.append(percent)
+    #plt.axis('tight')
+    #plt.legend(legend)
+    #plt.xlim([0, 5])
 
 # do a test frequency spectrum with know amplitudes
 sf = 1000
 x = np.linspace(0, 20, num=20*sf)
-y = 0.5*np.sin(2*np.pi*50.*x) + 1.*np.sin(2*np.pi*120.*x)
+y = 0.5*np.sin(2*np.pi*50.*x) + 2.*np.sin(2*np.pi*120.*x)
 f2, a2 = freq_spectrum(sf, y)
 plt.figure()
 plt.plot(f2, a2)

PythonCode/stats.py
@@ -30,7 +30,7 @@ qName = ['distance to rear wheel contact',
          'twist angle']
 
 # name the physical quantity, 'len' = length, 'ang' = angle
-qUnit = ['len', 'len', 'ang', 'ang', 'ang', 'ang', 'len', 'len', 'ang', 'len', 'len', 'len','ang', 'ang']
+qUnit = ['length', 'length', 'angle', 'angle', 'angle', 'angle', 'length', 'length', 'angle', 'length', 'length', 'length','angle', 'angle']
 
 # set to a single run for testing
 #runInfo['run'] = ['2002']
@@ -118,7 +118,7 @@ for j, speed in enumerate(UniqueSpeeds):
 vInt = [int(speed) for speed in nums.keys()]
 vInt.sort()
 lenYlabels = {'q':'Distance [m]', 'qd':'Rate [m/s]', 'qdd':'Acceleration [$m/s^2$]'}
-angYlabels = {'q':'Angel [deg]', 'qd':'Angular Rate [deg/s]', 'qdd':'AngularAcceleration [deg/$s^2$]'}
+angYlabels = {'q':'Angle [deg]', 'qd':'Angular Rate [deg/s]', 'qdd':'AngularAcceleration [deg/$s^2$]'}
 # for each of the states
 for i, name in enumerate(qName):
     qBox = {'q':[], 'qd':[], 'qdd':[]}
@@ -129,10 +129,10 @@ for i, name in enumerate(qName):
     for k in qBox.keys():
         fig = plt.figure()
         ax1 = fig.add_subplot(111)
-        if qUnit[i] == 'len':
+        if qUnit[i] == 'length':
             bp = plt.boxplot(qBox[k], notch=0, sym='', vert=1, whis=1.5, positions=vInt)
             plt.ylabel(lenYlabels[k])
-        elif qUnit[i] == 'ang':
+        elif qUnit[i] == 'angle':
             bp = plt.boxplot([180./np.pi*s for s in qBox[k]], notch=0, sym='', vert=1, whis=1.5, positions=vInt)
             plt.ylabel(angYlabels[k])
         else: